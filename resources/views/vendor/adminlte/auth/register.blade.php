3182e39571f391279b3d8c25b4185766195e924c
CAZA ZAGAL ANDREA SARAHI <128557831+sarahicaza@users.noreply.github.com>
2023-04-17 21:30:26 -0500

    Update register.blade.php
    cambio a español placeholder, boton y mensaje del registro

--- resources/views/vendor/adminlte/auth/register.blade.php
+++ resources/views/vendor/adminlte/auth/register.blade.php
@@ -11,7 +11,7 @@
     @php( $register_url = $register_url ? url($register_url) : '' )
 @endif
 
-@section('auth_header', __('adminlte::adminlte.register_message'))
+@section('auth_header', 'Nuevo registro')
 
 @section('auth_body')
     <form action="{{ $register_url }}" method="post">
@@ -20,7 +20,7 @@
         {{-- Name field --}}
         <div class="input-group mb-3">
             <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
-                   value="{{ old('name') }}" placeholder="{{ __('adminlte::adminlte.full_name') }}" autofocus>
+                   value="{{ old('name') }}" placeholder="Nombre y Apellido" autofocus>
 
             <div class="input-group-append">
                 <div class="input-group-text">
@@ -38,7 +38,7 @@
         {{-- Email field --}}
         <div class="input-group mb-3">
             <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
-                   value="{{ old('email') }}" placeholder="{{ __('adminlte::adminlte.email') }}">
+                   value="{{ old('email') }}" placeholder="Correo: ejemplo@ejemplo.com">
 
             <div class="input-group-append">
                 <div class="input-group-text">
@@ -56,7 +56,7 @@
         {{-- Password field --}}
         <div class="input-group mb-3">
             <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
-                   placeholder="{{ __('adminlte::adminlte.password') }}">
+                   placeholder="Contraseña">
 
             <div class="input-group-append">
                 <div class="input-group-text">
@@ -75,7 +75,7 @@
         <div class="input-group mb-3">
             <input type="password" name="password_confirmation"
                    class="form-control @error('password_confirmation') is-invalid @enderror"
-                   placeholder="{{ __('adminlte::adminlte.retype_password') }}">
+                   placeholder="Repita Contraseña">
 
             <div class="input-group-append">
                 <div class="input-group-text">
@@ -93,7 +93,7 @@
         {{-- Register button --}}
         <button type="submit" class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
             <span class="fas fa-user-plus"></span>
-            {{ __('adminlte::adminlte.register') }}
+            Registrarme
         </button>
 
     </form>
@@ -102,7 +102,7 @@
 @section('auth_footer')
     <p class="my-0">
         <a href="{{ $login_url }}">
-            {{ __('adminlte::adminlte.i_already_have_a_membership') }}
+            Ya estoy registrado
         </a>
     </p>
 @stop
